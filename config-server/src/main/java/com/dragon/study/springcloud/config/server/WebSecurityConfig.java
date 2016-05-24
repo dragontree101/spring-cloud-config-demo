b996c078ee3280846be3a9dce14758f27a7024af
longzhe <dragonlong1986@126.com>
2016-05-24 17:08:58 +0800

    update admin

--- config-server/src/main/java/com/dragon/study/springcloud/config/server/WebSecurityConfig.java
+++ config-server/src/main/java/com/dragon/study/springcloud/config/server/WebSecurityConfig.java
@@ -3,8 +3,10 @@ package com.dragon.study.springcloud.config.server;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /**
  * Created by dragon on 16/5/23.
@@ -12,11 +14,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      RequestMatcher csrfRequestMatcher = (request) -> false;
+      http.csrf().requireCsrfProtectionMatcher(csrfRequestMatcher);
+
+      http.authorizeRequests().antMatchers("/encrypt", "/encrypt/**", "/decrypt", "/decrypt/**").hasRole("ADMIN");
+      http.authorizeRequests().anyRequest().hasRole("USER");
+      http.httpBasic();
+    }
 
   @Override
   protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
-    authManagerBuilder.inMemoryAuthentication()
-        .withUser("user").password("password").roles("USER");
+    authManagerBuilder.inMemoryAuthentication().withUser("user").password("jF#{A{uCP4").roles("USER")
+        .and().withUser("admin").password("/tD+QA2[bz").roles("USER", "ADMIN");
   }
 }
